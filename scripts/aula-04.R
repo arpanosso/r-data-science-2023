e41059bc394f50356f211fdd9a0079045aa87ed0
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2023-12-07 07:17:37 -0300

    aula-04 m

---
+++ scripts/aula-04.R
@@ -0,0 +1,35 @@
+# Data:
+# Programado por:
+library(readxl)
+library(tidyverse)
+
+# Leia o arquivo geomorfologia.rds
+
+# Faça um gráfico de lonhas do teor de argila em função de x
+
+# Mapeie o tipo de solo por cor, e insira a superfície como
+# texto (label)
+
+## Adicione linhas verticais para demarcar as diferentes superfícies
+## adicione anotação definindo as superficies
+
+# Construir a matriz de correlação entre as variáveis
+
+# Escolha duas variáveis, contrua o gráfico de dispersão entre
+# elas e execute um teste no coeficiente de correlação
+# e posterior análise de regresão linear
+
+# construa os histograma para os teores de argila em cada
+# superficie e realize os teste de normalidade.
+
+## Estruturas de repetição
+## gerar sequências
+
+## Construir gráfico de dispersão para cada superfície
+## a partir das variáveis selecionadas anteriormente
+## usando o for
+
+# usando a map do pacote purrr.
+
+# Fatores no R
+
