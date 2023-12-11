64063f22adce337bcea12be0615cd847500631ea
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2023-12-10 22:20:38 -0300

    AULA 05

---
+++ scripts/aula-05.R
@@ -0,0 +1,22 @@
+# Data:
+# Programado por
+library(tidyverse)
+
+# Ler o banco de dados geomorfologia
+
+#  Criar uma tabela de contagem do número de observações
+# em cada categoria de SUP. Utilize a função count()
+
+# Construir um gráfico de colunas de superfície e tipos
+# de solo
+
+## Construir o gráfio de dispersão e adicionar a equação e
+## respectiva equação, use o pacote ggpubr
+
+# Gráfico com dois eixos y
+# x = X
+# y = ARGILA
+# y.sec = H_Al
+
+# Criar o gráfico de barras crescente da
+# contagem do número de solo
